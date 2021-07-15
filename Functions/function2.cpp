d131f382946a054ebe1ea1bb0554dab8d6a8d55a
silasplfilho <silasplfilho@outlook.com>
2021-07-15 10:08:51 -0300

    added new studdy points

---
+++ Functions/function2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+void introduceMe(string name) {
+    cout << "My name is " << name << endl;
+}
+
+void main(){
+    introduceMe("Silas");
+
+    system("pause>0");
+
+}
